145780719a1b03ec8c54d9f991508cd17d33af13
Olloyor Maxammatnabiyev <muhammadnabiyevolloyor@gmail.com>
2025-03-06 18:08:51 +0500

    Update settings.py

--- admin_panel/admin_panel/settings.py
+++ admin_panel/admin_panel/settings.py
@@ -80,6 +80,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '6543'),
     }
 }
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
